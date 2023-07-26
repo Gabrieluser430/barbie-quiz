baf1a749f8be4a71d67098d524a6d5d55cc82174
Gabriel Oliveira <gabrielbarbieri430@gmail.com>
2023-07-25 22:25:41 -0300

    Update page styling

--- index.js
+++ index.js
@@ -52,3 +52,31 @@ function handleAnswerSelection(selectedButton) {
         resultContainer.appendChild(resultImage);
         mainContainer.appendChild(resultContainer);
 }
+
+function saveToLocalStorage() {
+  const userInput = document.getElementById("userInput").value;
+  if (userInput.trim() !== "") {
+      let savedTopics = JSON.parse(localStorage.getItem("topics")) || [];
+      savedTopics.push(userInput);
+      localStorage.setItem("topics", JSON.stringify(savedTopics));
+      displaySavedTopics();
+      document.getElementById("userInput").value = "";
+  }
+}
+
+function displaySavedTopics() {
+  const savedTopics = JSON.parse(localStorage.getItem("topics")) || [];
+  const savedTopicsList = document.getElementById("savedTopics");
+
+  savedTopicsList.innerHTML = "";
+
+  savedTopics.forEach((topic) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = topic;
+      savedTopicsList.appendChild(listItem);
+  });
+}
+
+displaySavedTopics();
+
+localStorage.removeItem("topics")
